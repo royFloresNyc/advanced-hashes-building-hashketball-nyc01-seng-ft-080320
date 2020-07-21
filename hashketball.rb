74c12d6d1632d5ee7cc5238efc99fa1804b7e4e9
Roy  Flores <royfloresnyc@gmail.com>
2020-07-21 22:32:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,62 @@
-# Write your code here!
+
+
+def game_hash
+  result_hash = {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+            :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        }
+      ]
+    },
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+          :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        },
+        {
+            :player_name => "",
+          :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks =>
+        }
+      ]
+    }
+  }
+end
