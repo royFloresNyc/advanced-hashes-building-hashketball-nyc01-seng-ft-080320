da53825f0ec1f2e93800506afd33dd632c38b247
Roy  Flores <royfloresnyc@gmail.com>
2020-07-21 22:36:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,7 +18,18 @@ def game_hash
           :slam_dunks => 1 
         },
         {
-            :player_name => "",
+          :player_name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
+        },
+        {
+          :player_name => "",
           :number => ,
           :shoe => ,
           :points => ,
